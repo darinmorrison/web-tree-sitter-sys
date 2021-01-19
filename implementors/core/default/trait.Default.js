c4ef2bd40ed1ed9979e6f3d65774840554cccfb2
hvithrafn <hvithrafn@users.noreply.github.com>
2021-01-19 04:16:42 +0000

    deploy: 6eba8ca8f6a627a7d602d9264dc78fe96b27c321

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["web_tree_sitter_sys"] = [{"text":"impl Default for Edit","synthetic":false,"types":[]},{"text":"impl Default for ParseOptions","synthetic":false,"types":[]},{"text":"impl Default for Point","synthetic":false,"types":[]},{"text":"impl Default for PredicateOperand","synthetic":false,"types":[]},{"text":"impl Default for PredicateResult","synthetic":false,"types":[]},{"text":"impl Default for Range","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
